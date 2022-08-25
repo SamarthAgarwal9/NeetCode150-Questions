e97af57ca662efc870841f17b35d50c9bff5e93d
Samarth Agarwal <102257671+SamarthAgarwal9@users.noreply.github.com>
2022-08-26 01:44:19 +0530

    Time: 22 ms (77.17%), Space: 65.3 MB (46.90%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
@@ -4,34 +4,19 @@ class Solution {
         if(s.length()%2!=0)return false;
         int remain = 0;
     for (int i = 0; i < n; i++) {
-      // check locked ')'
-      if (locked.charAt(i) == '1' && s.charAt(i) == ')') {
-        // don't have nothing left to pair ')'
-        if (remain == 0) {
-          return false;
-        } else {
-          remain--;
-        }
-      } else {
-        remain++;
-      }
+     if (locked.charAt(i) == '1' && s.charAt(i) == ')') {
+        if (remain == 0)return false;
+        else remain--;
+        } 
+    else remain++;
     }
-
-    // backward check '('
-    // number of unlocked and locked ')'
-    remain = 0;
+     remain = 0;
     for (int i = n - 1; i >= 0; i--) {
-      // check locked '('
       if (locked.charAt(i) == '1' && s.charAt(i) == '(') {
-        // don't have nothing left to pair '('
-        if (remain == 0) {
-          return false;
-        } else {
-          remain--;
-        }
-      } else {
-        remain++;
-      }
+        if (remain == 0) return false;
+         else  remain--;
+      } 
+        else remain++;
     }
 
     return true;
