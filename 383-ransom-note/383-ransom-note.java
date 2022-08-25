2994a8738e5086ce3ec6ab5e8feff37c45e40cf4
Samarth Agarwal <102257671+SamarthAgarwal9@users.noreply.github.com>
2022-08-25 12:15:31 +0530

    Time: 25 ms (38.13%), Space: 52.8 MB (20.40%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int s1=ransomNote.length(),s2=magazine.length();
+        if(s1>s2)return false;
+        Map<Character,Integer> m1=new HashMap<>();
+        Map<Character,Integer> m2=new HashMap<>();
+        for(char c:ransomNote.toCharArray())
+            m1.put(c,m1.getOrDefault(c,0)+1);
+        for(char c:magazine.toCharArray())
+            m2.put(c,m2.getOrDefault(c,0)+1);
+        for(char c:m1.keySet()){
+            if(m2.containsKey(c))
+                if(m2.get(c)<m1.get(c))
+                    return false;
+                else continue;
+            else return false;
+        }
+        return true;
+    }
+}
