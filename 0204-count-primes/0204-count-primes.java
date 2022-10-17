ff0726887f4ce3b9221b8e586101fb95327917fb
Samarth Agarwal <102257671+SamarthAgarwal9@users.noreply.github.com>
2022-10-18 03:02:41 +0530

    Time: 193 ms (44.04%), Space: 68.3 MB (64.72%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -3,16 +3,12 @@ class Solution {
         if(n==0||n==1)return 0;
         int res=0;
         boolean[] vis=new boolean[n];
-        for(int i=2;i<n;i++){
+        for(int i=2;i<n;i++)
             if(vis[i]==false){
                 res++;
-                for(int j=2;i*j<n;j++){
+                for(int j=2;i*j<n;j++)
                     vis[j*i]=true;
-                }
             }
-            
-        }
-    
         return res;
     
     }
