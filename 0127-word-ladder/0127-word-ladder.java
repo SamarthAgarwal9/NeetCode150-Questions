61bad38109b011d4961eb931709ac8ec2184572b
Samarth Agarwal <102257671+SamarthAgarwal9@users.noreply.github.com>
2022-11-03 14:32:11 +0530

    Time: 237 ms (35.10%), Space: 118.7 MB (5.21%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,35 +1,6 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-//         Set<String> set=new HashSet<>(wordList);
-//         if(!set.contains(endWord))return 0;
-//         Queue<String> que=new LinkedList<>();
-//         Set<String> set1=new HashSet<>();
-//         que.add(beginWord);
-//         set1.add(beginWord);
-//         int res=1;
-//         while(!que.isEmpty()){
-//             int size=que.size();
-//             for(int i=0;i<size;i++){
-//                 String s=que.poll();
-//                 if(s.equals(endWord))return res;
-//                 for(int j=0;j<s.length();j++){
-//                     for(int k='a';k<='z';k++){
-//                         char[] ch=s.toCharArray();
-//                         ch[j]=(char)k;
-                        
-//                         String demo=new String(ch);
-//                         if(!que.contains(demo)&&set.contains(demo)){
-//                             que.add(demo);
-//                             set1.add(demo);
-//                         }
-//                     }
-//                 }
-//             }
-//                 res++;
-            
-//         }
-//         return 0;
-//     }
+
         Set<String> set = new HashSet<>(wordList);
         if(!set.contains(endWord)) return 0;
         
@@ -37,7 +8,7 @@ class Solution {
         queue.add(beginWord);
         
         Set<String> visited = new HashSet<>();
-        queue.add(beginWord);
+        visited.add(beginWord);
         
         int changes = 1;
         
